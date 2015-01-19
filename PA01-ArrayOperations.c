4bd0d3240384b50300a9905913c625628771419f
Brian Rieder <brieder777@users.noreply.github.com>
2015-01-19 15:35:19 -0500

    Full PA01 solution

---
+++ PA01-ArrayOperations.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int arraySum(int * array, int len)
+{
+	int i, sum = 0;
+	for(i = 0; i < len; ++i) 
+		sum += array[i];
+	return sum;
+}
+
+int arrayCountNegative(int * array, int len)
+{
+	int i, count = 0;
+	for(i = 0; i < len; ++i)
+		if(array[i] < 0)
+			++count;
+	return count;
+}
+
+int arrayIsIncreasing(int * array, int len)
+{
+	int i;
+	for(i = 1; i < len; ++i)
+		if(array[i] < array[i - 1])
+			return FALSE;
+	return TRUE;
+}
+
+int arrayIndexRFind(int needle, const int * haystack, int len)
+{
+	int i;
+	for(i = len - 1; i >= 0; --i)
+		if(haystack[i] == needle)
+			return i;
+	return -1;
+}
+
+int arrayFindSmallest(int * array, int len)
+{
+	int i, min_index = 0;
+	for(i = 1; i < len; ++i)
+		if(array[i] < array[min_index])
+			min_index = i;
+	return min_index;
+}
